400a8d9b7bbac31d396e7b7db8852e87ccda1c7b
Cesar Victorio <cbvictorio@gmail.com>
2025-10-10 21:49:16 -0700

    Add logrus logging to users table migration
    Integrated logrus for improved logging in the users table migration script. The log formatter is set for colored output and full timestamps, and a log message is added to indicate successful table deletion and synchronization.

--- golang/gin-example/go.mod
+++ golang/gin-example/go.mod
@@ -37,6 +37,7 @@ require (
 	github.com/quic-go/qpack v0.5.1 // indirect
 	github.com/quic-go/quic-go v0.55.0 // indirect
 	github.com/radovskyb/watcher v1.0.7 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	go.uber.org/mock v0.6.0 // indirect
--- golang/gin-example/migrations/users_table.go
+++ golang/gin-example/migrations/users_table.go
@@ -5,17 +5,26 @@ import (
 	"gin-example/internal/models"
 	"path/filepath"
 	"runtime"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func init() {
 	_, currentFilePathname, _, _ := runtime.Caller(0)
 	basepath := filepath.Dir(currentFilePathname)
 	envPath := filepath.Join(basepath, "../configs/.env")
+
 	initializers.LoadEnvVariables(envPath)
 	initializers.ConnectToDatabase()
+
+	log.SetFormatter(&log.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: true,
+	})
 }
 
 func main() {
 	initializers.DB.Migrator().DropTable(&models.User{})
 	initializers.DB.AutoMigrate(&models.User{})
+	log.Info("User Table deleted and synced successfully")
 }
